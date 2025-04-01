e7bfa44f8983fcedfeeeb9512019f92529b814f3
Gavin Szczesniak <gavinszczesniak@gmail.com>
2025-04-01 19:56:20 -0400

    Adds very basic recursion for the createElement method   - Plan to change method names later on to not get confused with the "document" method names

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-function createElement(tagName, props, ...children) {
+function createElement(tagName, props, children) {
   if (tagName) {
+    console.log(tagName, props, children)
+
     const tagElement = document.createElement(tagName);
     
-    if (children.length) {
-      for (let i = 0; i < children.length; i++) {
-        if (children[i] === "p") {
-          const childElement = document.createElement(children[i])
-          const textNode = document.createTextNode("I am a child p element");
+    if (props.text) {
+      const textNode = document.createTextNode(props.text);
+      tagElement.append(textNode);
+    }
 
-          childElement.append(textNode);
-          tagElement.appendChild(childElement);
-        }
-      }
+    for (const child of children) {
+      console.log(child, child.attributes, child.children)
+      const childElement = createElement(child.parent, child.attributes, child.children);
+      console.log(childElement);
+      tagElement.appendChild(childElement);
     }
 
     return tagElement;
@@ -24,19 +26,43 @@ function createTextElement(text) {
 
 // TODO Make the renderer!!!
 function render(component) {
+  console.log(component)
   const dom = document.getElementById("app");
 
-  const formattedDOM = createElement("div", {}, "p");
+  const formattedDOM = createElement(component.parent, component.attributes, component.children);
   console.log(formattedDOM);
 
   dom.append(formattedDOM)
 }
 
 const CoolComponent = () => {
-  
+  return {
+    parent: "div",
+    attributes: {
+      text: ""
+    },
+    children: [
+      {
+        parent: "p",
+        attributes: {
+          text: "I am a cool component"
+        },
+        children: [
+          {
+            parent: "span",
+            attributes: {
+              text: "I AM SPAN!"
+            },
+            children: []
+          }
+        ]
+      },
+    ]
+  }
 }
 
-render();
+const componentData = CoolComponent();
+render(componentData);
 
 // Store element component
 // render onto the root element
