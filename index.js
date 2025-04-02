ab406151a8330ca610f8f9da72beceac409b4bce
Gavin Szczesniak <gavinszczesniak@gmail.com>
2025-04-01 20:47:30 -0400

    Refactors the name of the methods   - Adds JSDoc to add clarity to what the methods are doing and type enforcement   - Adds another element of button to prepare for creating a custom event listener when using onClick   - Next plan is to find a way to parse straight from HTML to add attributes like "onClick"=...

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-function createElement(tagName, props, children) {
+/**
+ * Recursively apply the elements from the component to the parent element of the component
+ * @param {string} tagName - The HTML element to be used on the specific tree
+ * @param {Object} props - Props the element would contain
+ * @param {Object[]} children - Child element(s) of the parent element (if any)
+ * @returns {Object} - The element that was constructed
+ */
+function applyNode(tagName, props, children) {
   if (tagName) {
-    console.log(tagName, props, children)
-
     const tagElement = document.createElement(tagName);
     
+    // If the element contains text in the attribute, add text to the element
     if (props.text) {
-      const textNode = document.createTextNode(props.text);
-      tagElement.append(textNode);
+      tagElement.append(applyingNodeText(props.text));
     }
 
     for (const child of children) {
-      console.log(child, child.attributes, child.children)
-      const childElement = createElement(child.parent, child.attributes, child.children);
-      console.log(childElement);
+      // Recursively create the child element, its attributes, and recursively for the children too
+      const childElement = applyNode(child.parent, child.attributes, child.children);
       tagElement.appendChild(childElement);
     }
 
+    // Return the Node tree to then be rendered to the user
     return tagElement;
   }
 }
 
-function createTextElement(text) {
-
+/**
+ * Apply text to the node if they have text within their properties
+ * @param {string} text - The text that will be appended to the element
+ * @returns {Object} - The text that is created from the text node
+ */
+function applyingNodeText(text) {
+  return document.createTextNode(text);
 }
 
-// TODO Make the renderer!!!
+/**
+ * Apply the elements to form the node tree of the DOM to then be rendered to the page
+ * @param {Object} component - The tree to build to the DOM
+ */
 function render(component) {
-  console.log(component)
   const dom = document.getElementById("app");
 
-  const formattedDOM = createElement(component.parent, component.attributes, component.children);
-  console.log(formattedDOM);
+  const formattedDOM = applyNode(component.parent, component.attributes, component.children);
 
+  // TODO:
+  // Will come back to this since we could have more than one component at a time
   dom.append(formattedDOM)
 }
 
@@ -45,7 +58,7 @@ const CoolComponent = () => {
       {
         parent: "p",
         attributes: {
-          text: "I am a cool component"
+          text: "Counter: "
         },
         children: [
           {
@@ -57,6 +70,14 @@ const CoolComponent = () => {
           }
         ]
       },
+      {
+        parent: "button",
+        attributes: {
+          text: "Add 1"
+          // Need to make a event listener to track button clicks
+        },
+        children: []
+      }
     ]
   }
 }
@@ -64,6 +85,8 @@ const CoolComponent = () => {
 const componentData = CoolComponent();
 render(componentData);
 
+
+// -------- Planning -----------
 // Store element component
 // render onto the root element
 
